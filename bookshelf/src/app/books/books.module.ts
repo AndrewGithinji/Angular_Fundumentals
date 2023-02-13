312ff2e20abe230c05a81dfb9a6dd548689a5ede
AndrewGithinji <andrewgithinji@hotmail.com>
2023-02-13 12:19:03 +0300

    Books Components

---
+++ bookshelf/src/app/books/books.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BooklistComponent } from './booklist/booklist.component';
+
+
+
+@NgModule({
+  declarations: [
+    BooklistComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BooksModule { }
