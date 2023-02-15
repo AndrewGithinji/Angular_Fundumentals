6b251ea8e234b358d6d6abe22e1f8f03048f903b
AndrewGithinji <andrewgithinji@hotmail.com>
2023-02-15 10:55:02 +0300

    ng Directive to Iterate over a list

---
+++ my-app/src/app/zoo/zoo.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zoo',
+  templateUrl: './zoo.component.html',
+  styleUrls: ['./zoo.component.css']
+})
+export class ZooComponent implements OnInit {
+  animalList: object[] = [];
+
+  constructor() { }
+
+ngOnInit(): void {
+this.animalList = [
+  {
+    id: 1,
+    name: 'Panda',
+    fed: true
+  },
+  {
+    id: 2,
+    name: 'Elephant',
+    fed: true
+  },
+  {
+    id: 3,
+    name: 'Lion',
+    fed: false
+  }
+]
+  }
+
+};
