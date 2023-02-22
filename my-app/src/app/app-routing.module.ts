9d85d567eefadd99097d8346819ce0f33f6283a4
AndrewGithinji <andrewgithinji@hotmail.com>
2023-02-22 13:08:29 +0300

    linking components

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LinksComponent } from './links/links.component';
+import { AboutComponent } from './about/about.component';
+const routes: Routes = [
+  { path: '', component:LinksComponent },
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
